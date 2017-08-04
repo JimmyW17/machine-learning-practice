10030422136f5bfea53bf161fbf791bdf45fabba
Jimmy Wu <wujimmy94@gmail.com>
2017-08-04 14:13:26 -0400

    added pry for debugging

--- linear_regression.rb
+++ linear_regression.rb
@@ -1,14 +1,16 @@
 require 'csv'
 require 'ruby_linear_regression'
+require 'pry'
 
 x_data = []
 y_data = []
+
 # Load data from CSV file into two arrays - one for independent variables X and one for the dependent variable Y
 # Each row contains square feet for property and living area like this:
 # [ SQ FEET PROPERTY, SQ FEET HOUSE ]
 CSV.foreach("./data/staten-island-single-family-home-sales-2015.csv", :headers => true) do |row|
-x_data.push( [row[0].to_i, row[1].to_i] )
-y_data.push( row[2].to_i )
+  x_data.push( [row[0].to_i, row[1].to_i] )
+  y_data.push( row[2].to_i )
 end
 
 # Create regression model
@@ -20,6 +22,8 @@ linear_regression.load_training_data(x_data, y_data)
 linear_regression.train_normal_equation
 
 # Predict the price of a 2000 sq feet property with a 1500 sq feet house
-prediction_data = [2000, 1500]
+prediction_data = [10000, 7500]
+sqft_property = prediction_data[0]
+sqft_house = prediction_data[1]
 predicted_price = linear_regression.predict(prediction_data)
-puts "Predicted selling price for a 1500 sq feet house on a 2000 sq feet property: #{predicted_price.round}$"
+puts "Predicted selling price for a #{sqft_house} sq feet house on a #{sqft_property} sq feet property: #{predicted_price.round}"
